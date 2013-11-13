416efd2469be81c89f821af46a5d75b23e442c57
Courtney Goeltzenleuchter <courtney@LunarG.com>
2013-11-13 11:11:39 -0700

    mesa: Add gl_ViewportIndex variable to GLSL

--- src/glsl/glsl_parser_extras.h
+++ src/glsl/glsl_parser_extras.h
@@ -356,6 +356,7 @@ struct _mesa_glsl_parse_state {
    bool EXT_shader_integer_mix_warn;
    bool ARB_shader_atomic_counters_enable;
    bool ARB_shader_atomic_counters_warn;
+   bool ARB_viewport_array_enable;
    /*@}*/
 
    /** Extensions supported by the OpenGL implementation. */
